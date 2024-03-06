36d4fa37ef37248f5c1926fecb1286df88e5f5eb
Alexandre Borges <alexandre.bsilva55@gmail.com>
2024-03-05 22:11:46 -0300

    first commit

---
+++ src/iindex.js
@@ -0,0 +1,13 @@
+// Find the <a> element with the specific href
+const followingModalLink = document.querySelector(
+  'a[href="/alexandre.borges.io/following/"]'
+);
+const followersModalLink = document.querySelector(
+  'a[href="/alexandre.borges.io/followers/"]'
+);
+const modalQuerySelector = "._aano";
+
+const followingModal = document.querySelector("._aano");
+
+// Log the found element to the console
+console.log(followersModalLink, followingModalLink);
