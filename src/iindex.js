0c75115c5114e3f45cc5ea68e1acb749d1450163
Alexandre Borges <alexandre.bsilva55@gmail.com>
2024-03-05 22:12:29 -0300

    to be continued - WIP

--- src/iindex.js
+++ src/iindex.js
@@ -1,13 +1,15 @@
-// Find the <a> element with the specific href
 const followingModalLink = document.querySelector(
   'a[href="/alexandre.borges.io/following/"]'
 );
 const followersModalLink = document.querySelector(
   'a[href="/alexandre.borges.io/followers/"]'
 );
+
 const modalQuerySelector = "._aano";
 
 const followingModal = document.querySelector("._aano");
 
 // Log the found element to the console
 console.log(followersModalLink, followingModalLink);
+
+//... to be continued
